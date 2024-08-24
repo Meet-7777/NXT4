841cc66245e19a78f6541ae4e99a6ad0433b36f2
Meet-7777 <20104057@apsit.edu.in>
2024-08-24 22:25:10 +0530

    Removed Vite+react-SVG

--- my-react-app/src/MainRouter.jsx
+++ my-react-app/src/MainRouter.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './App'; // CSK Slotted component
-import CskPhliphs from './CskPhliphs'; // CSK Philips component
-import Layout from './Layout'; // Layout component
+import App from './App';
+import CskPhliphs from './CskPhliphs';
+import Layout from './Layout'; 
 
 function MainRouter() {
     return (
         <Router>
             <Layout>
                 <Routes>
-                    <Route path="/" element={<App />} /> {/* Route for CSK Slotted */}
-                    <Route path="/csk-philiphs" element={<CskPhliphs />} /> {/* Route for CSK Philips */}
+                    <Route path="/" element={<App />} /> 
+                    <Route path="/csk-philiphs" element={<CskPhliphs />} /> 
                 </Routes>
             </Layout>
         </Router>
